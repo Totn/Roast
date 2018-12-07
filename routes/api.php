e67691a10a7ca44705c4d3b1bdff50e9d4946fce
Totn Lee <letotn@gmail.com>
2018-12-07 21:40:22 +0800

    roast第七章，实现后端接口API，cafes/cafe/NewCafe

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,37 @@ Route::group(['prefix' => 'v1', 'middleware' => 'auth:api'], function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+
+    /*
+    |--------------------------------------------------------------------------
+    | Get All Cafes
+    |--------------------------------------------------------------------------
+    | URL:  /api/v1/cafes
+    | Controller: API\CafesController@getCafes
+    | Method: GET
+    | Description: Gets all of the cafes in the application
+    */
+    Route::get('/cafes', 'API\CatesController@getCafes');
+
+    /*
+    |--------------------------------------------------------------------------
+    | Add a New Cafe
+    |--------------------------------------------------------------------------
+    | URL:  /api/v1/cafes
+    | Controller: API\CafesController@postNewCafe
+    | Method: POST
+    | Description: Adds a new cafe to the application
+    */
+    Route::post('/cafes', 'API\CafesController@postNewCafe');
+
+    /*
+    |--------------------------------------------------------------------------
+    | Get An Individual Cafe
+    |--------------------------------------------------------------------------
+    | URL:  /api/v1/cafes/{id}
+    | Controller: API\CafesController@getCafe
+    | Method: GET
+    | Description: Get An Individual Cafe
+    */
+    Route::get('/cafes/:id', "API\CafesController@getCafe");
 });
